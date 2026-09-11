6e21720a819f1ce27bf2c80497cd0c939689baa9
Aqua-218 <keiya_26n1102992@nnn.ed.jp>
2026-09-11 14:09:50 +0900

    feat: 境内の生成を Worker で行う
    生成に数百 ms かかるので、メインスレッドを止めないよう Worker へ出す。
    頂点はコピーせず転送する。

---
+++ js/build-worker.js
@@ -0,0 +1,9 @@
+// 境内の生成をメインスレッドから外す。生成した頂点はコピーせずに転送する
+import { buildScene } from './shrine.js';
+import { noiseTexture } from './noise.js';
+
+onmessage = e => {
+  const scene = buildScene(e.data.seed);
+  scene.noise = noiseTexture();
+  postMessage(scene, [scene.vertices.buffer, scene.blossoms.buffer, scene.petals.buffer, scene.noise.buffer]);
+};
